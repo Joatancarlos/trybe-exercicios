a3f96464c4f8adb94025120d6c3a23202360a49f
Joatan Feitosa <joatancarlos@gmail.com>
2023-01-26 15:47:51 -0300

    primeiro exercício do dia

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-de-programacao/exercicio-fatoracao.js
@@ -0,0 +1,8 @@
+// crie um algoritmo que imprima na tela o fatorial de 10
+let fatorial = 5
+let resultado = fatorial
+for (let i = 1; i < fatorial; i += 1) {
+    resultado *= i
+}
+
+console.log(resultado);
