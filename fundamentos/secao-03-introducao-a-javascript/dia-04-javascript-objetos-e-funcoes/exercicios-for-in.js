c9f430aadcbf838d014027c84231babc94cc58fa
Joatan Feitosa <joatancarlos@gmail.com>
2023-01-26 18:58:18 -0300

    exercício foguetinho finalizado

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-for-in.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-for-in.js
@@ -17,3 +17,31 @@ for (caracteristicas in info) {
 for (caracteristicas in info) {
     console.log(info[caracteristicas]);
 }
+
+// Exercício foguetinho
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`);
+
+  leitor.livrosFavoritos[1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+
+  console.log(leitor);
+  
+
+  console.log(`Julia tem ${Math.floor(Math.floor(Math.random() * 10))} livros favoritos`);
